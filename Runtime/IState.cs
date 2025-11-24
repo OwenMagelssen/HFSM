c617a19cf2af3139eedc2c59af08ac1987d51b48
Owen Magelssen <44145090+OwenMagelssen@users.noreply.github.com>
2025-11-24 03:50:14 -0800

    IState helper function default implementations

--- Runtime/IState.cs
+++ Runtime/IState.cs
@@ -14,5 +14,44 @@ namespace HFSM
 		public IState iParent { get; }
 		public IState iDefaultSubState { get; }
 		public IState iActiveSubState { get; }
+		
+		public IState NearestCommonAncestorWith(IState state)
+		{
+			IState a = iParent;
+			IState b = state.iParent;
+
+			while (a != null && b != null)
+			{
+				if (a == b) return a;
+				a = a.iParent;
+				b = b.iParent;
+			}
+
+			return null;
+		}
+
+		public bool IsSiblingOf(IState state)
+		{
+			return state.iParent == iParent;
+		}
+
+		public bool IsDescendantOf(IState state)
+		{
+			return state.IsAncestorOf(this);
+		}
+
+		public bool IsAncestorOf(IState state)
+		{
+			if (state == null) return false;
+			IState ancestor = state.iParent;
+
+			while (ancestor != null)
+			{
+				if (ancestor == this) return true;
+				ancestor = ancestor.iParent;
+			}
+
+			return false;
+		}
 	}
 }
