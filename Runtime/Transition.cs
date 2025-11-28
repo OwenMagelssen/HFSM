a392328fc629fed715d485e7d8106fd45ecb0849
Owen Proto <44145090+OwenMagelssen@users.noreply.github.com>
2025-11-28 02:06:30 -0800

    added transition names

--- Runtime/Transition.cs
+++ Runtime/Transition.cs
@@ -11,11 +11,13 @@ namespace HFSM
 	public abstract class Transition<T> where T : StateData
 	{
 		public State<T> DestinationState { get; private set; }
+		public readonly string Name;
 
-		public Transition(State<T> destinationState)
+		public Transition(State<T> destinationState, string name = null)
 		{
 			if (destinationState == null) Debug.LogError("DestinationState of a Transition cannot be null");
 			DestinationState = destinationState;
+			Name = string.IsNullOrEmpty(name) ? "To " + DestinationState.Name : name;
 		}
 
 		public abstract bool TryTransition();
@@ -25,7 +27,8 @@ namespace HFSM
 	{
 		private readonly Func<bool> _condition;
 
-		public ConditionTransition(State<T> destinationState, Func<bool> condition) : base(destinationState)
+		public ConditionTransition(State<T> destinationState, Func<bool> condition, string name = null) 
+			: base(destinationState, name)
 		{
 			_condition = condition;
 		}
